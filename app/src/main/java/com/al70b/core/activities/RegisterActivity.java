28690d87b8c640a10322c180481e2517b826ed73
Naseem Hajoj <naseemhajoj@gmail.com>
2016-08-25 18:58:11 +0300

    Replace register fragment with an activity, adjust home page layout and few gui changes

---
+++ app/src/main/java/com/al70b/core/activities/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.al70b.core.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
---
+++ app/src/main/java/com/al70b/core/adapters/GuestHomePageAdapter.java
@@ -0,0 +1,7 @@
+package com.al70b.core.adapters;
+
+/**
+ * Created by nasee on 8/25/2016.
+ */
+public class GuestHomePageAdapter {
+}
